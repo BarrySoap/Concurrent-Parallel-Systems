d9f7ece3ed2d8288bac751a2914de39c05a71c00
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-09-24 14:27:34 +0100

    New class for shared resources inspection.

---
+++ CPSLabs/Labs/Lab02.cpp
@@ -0,0 +1,34 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+using namespace std::this_thread;
+
+void increment(shared_ptr<int> value)
+{
+	// Loop 1 million times, incrementing value
+	for (unsigned int i = 0; i < 1000000; ++i)
+		// Increment value
+		*value = *value + 1;
+}
+
+int main(int argc, char **argv)
+{
+	// Create a shared int value
+	auto value = make_shared<int>(0);
+
+	// Create number of threads hardware natively supports
+	auto num_threads = thread::hardware_concurrency();
+	vector<thread> threads;
+	for (unsigned int i = 0; i < num_threads; ++i)
+		threads.push_back(thread(increment, value));
+
+	// Join the threads
+	for (auto &t : threads)
+		t.join();
+
+	// Display the value
+	cout << "Value = " << *value << endl;
+}
