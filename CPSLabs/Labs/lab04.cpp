9c3252781325c32786322670c014fc6b9480af66
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-10-08 14:43:42 +0100

    Lab 4 finished.

--- CPSLabs/Labs/lab04.cpp
+++ CPSLabs/Labs/lab04.cpp
@@ -2,6 +2,7 @@
 #include <immintrin.h>
 #include <cassert>
 #include <chrono>
+#include <random>
 
 using namespace std;
 using namespace std::chrono;
@@ -17,6 +18,47 @@ union v8
 	__m256 v;
 	float a[8];
 };
+
+void generate_data(v4 *data, size_t n)		// Randomly generate vector values
+{
+	random_device r;						// Create random engine
+	default_random_engine e(r());
+
+	for (size_t i = 0; i < n; ++i) {		// Fill data
+		for (size_t j = 0; j < 4; ++j) {
+			data[i].a[j] = e();
+		}
+	}
+}
+
+void normalise_vectors(v4 *data, v4 *result, size_t n)			// Normalises the vector
+{
+	for (size_t i = 0; i < n; ++i) {							// Normalise the vectors
+		result[i].v = _mm_mul_ps(data[i].v, data[i].v);			// Square each component - simply multiply the vectors by themselves
+		result[i].v = _mm_hadd_ps(result[i].v, result[i].v);	// Calculate sum of the components.
+		result[i].v = _mm_hadd_ps(result[i].v, result[i].v);	// See notes to explain hadd.
+
+		result[i].v = _mm_rsqrt_ps(result[i].v);				// Calculate reciprocal square root of the values
+																// That is 1.0f / sqrt(value) - or the length of the vector
+
+		result[i].v = _mm_mul_ps(data[i].v, result[i].v);		// Multiply result by the original data
+																// As we have the reciprocal it is the same as dividing each component by the length
+	}															// All vectors now normalised
+}
+
+void check_results(v4 *data, v4 *result)						// Check the first 100 results
+{
+	for (size_t i = 0; i < 100; ++i) {							// Check first 100 values
+		float l = 0.0f;											// Calculate the length of the vector
+		for (size_t j = 0; j < 4; ++j) {
+			l += powf(data[i].a[j], 2.0f);						// Square each component and add to l
+		}
+		l = sqrtf(l);											// Get sqrt of the length
+		for (size_t j = 0; j < 4; ++j) {
+			cout << data[i].a[j] / l << " : " << result[i].a[j] << endl;	// Now check that the individual results
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -55,7 +97,7 @@ int main(int argc, char **argv)
 	return 0;
 	/*************************************/
 
-	/* Example 2 - SIMD Operations */
+	/* Example 2 - SIMD Operations /
 	{											// Test all types to ensure add is working
 		alignas(16) v4 x;
 		alignas(16) v4 y;
@@ -122,6 +164,17 @@ int main(int argc, char **argv)
 		_aligned_free(d1);
 		_aligned_free(d2);
 	}
+	return 0;
+	/*************************************/
+
+	/* Example 3 - Vector Normalisation */
+	v4 *data = (v4*)_aligned_malloc(sizeof(v4) * 1000000, 16);
+	v4 *result = (v4*)_aligned_malloc(sizeof(v4) * 1000000, 16);
+	generate_data(data, 1000000);
+	normalise_vectors(data, result, 1000000);
+	check_results(data, result);
+	_aligned_free(data);
+	_aligned_free(result);
 	return 0;
 	/*************************************/
 }
