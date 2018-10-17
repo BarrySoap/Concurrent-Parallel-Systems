a1882d7cdf10a4a6a5432c83c579f272f2cd2817
Glenn Sullivan <glennpws@hotmail.co.uk>
2018-10-17 22:40:44 +0100

    TOTALLY NOT HACKY R CODE BUT IT WORKS HAH

--- Plot.R
+++ Plot.R
@@ -1,3 +1,3 @@
 library(ggplot2)
-data <- ggplot(CourseworkTest, aes(x=Individual.Block.Times, y = Index)) + geom_point()
+data <- ggplot(CourseworkTest, aes(x=as.numeric(as.character(Individual.Block.Times)), y = Index)) + geom_point() + scale_x_continuous(limits = c(0.0, 0.2))
 print(data)
