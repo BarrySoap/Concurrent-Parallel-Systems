2d4e55c031bea581747ad2ca02b4c85d329b0b37
Glenn Sullivan <glennpws@hotmail.co.uk>
2018-10-17 22:05:34 +0100

    Added a template for facet wrapping in R. Should be able to plot multiple graphs with this eventually.

---
+++ facetWrapExampleScript.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+gg <- ggplot(diamonds, aes(x=carat, y=price, color=cut)) + geom_point() + labs(title="Scatterplot", x="Carat", y="Price")  # add axis lables and plot title.
+print(gg)
+
+gg1 <- gg + theme(plot.title=element_text(size=30, face="bold"), 
+                  axis.text.x=element_text(size=15), 
+                  axis.text.y=element_text(size=15),
+                  axis.title.x=element_text(size=25),
+                  axis.title.y=element_text(size=25)) + 
+  scale_color_discrete(name="Cut of diamonds")  # add title and axis text, change legend title.
+print(gg1)
+
+gg1 + facet_wrap( ~ cut, ncol=3)
