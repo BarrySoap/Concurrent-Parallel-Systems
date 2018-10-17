32276991b59ac7f08f91882da34f89a911da8ebd
Glenn Sullivan <glennpws@hotmail.co.uk>
2018-10-17 22:46:03 +0100

    Added axes labeling.

--- Plot.R
+++ Plot.R
@@ -1,3 +1,3 @@
 library(ggplot2)
-data <- ggplot(CourseworkTest, aes(x=as.numeric(as.character(Individual.Block.Times)), y = Index)) + geom_point() + scale_x_continuous(limits = c(0.0, 0.2))
+data <- ggplot(CourseworkTest, aes(x=as.numeric(as.character(Individual.Block.Times)), y = Index)) + geom_point() + scale_x_continuous(limits = c(0.0, 0.2)) + labs(title="Block Mining Time", x="Time Taken", y="Block Index")
 print(data)
