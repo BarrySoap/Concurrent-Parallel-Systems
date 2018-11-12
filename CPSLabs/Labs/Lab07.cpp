fd78fd5391c4018c50e6e05d129733ea3a196ddd
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-11-12 14:56:57 +0000

    Second example, printing OpenCL info.

--- CPSLabs/Labs/Lab07.cpp
+++ CPSLabs/Labs/Lab07.cpp
@@ -9,6 +9,7 @@ using namespace cl;
 
 int main(int argc, char **argv)
 {
+	/* Exercise 1: Print onboard GPU /
 	try
 	{
 		// Get the platforms
@@ -33,4 +34,46 @@ int main(int argc, char **argv)
 		cout << error.what() << "(" << error.err() << ")" << endl;
 	}
 	return 0;
+	/***********************************/
+
+	/* Exercise 2: Print OpenCL Info */
+	try
+	{
+		// Get the platforms
+		vector<Platform> platforms;
+		Platform::get(&platforms);
+
+		// Iterate through each platform
+		for (auto &p : platforms)
+		{
+			// Print out platform name
+			cout << "********************" << endl;
+			cout << p.getInfo<CL_PLATFORM_NAME>() << endl;
+
+			// Get all devices for the platform
+			vector<Device> devices;
+			p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+			// Iterate through all the devices
+			for (auto &d : devices)
+			{
+				cout << endl;
+				cout << d.getInfo<CL_DEVICE_NAME>() << endl;
+				cout << "Vendor: " << d.getInfo<CL_DEVICE_VENDOR>() << endl;
+				cout << "Max Compute: " << d.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << endl;
+				cout << "Max Memory: " << d.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>() / (1024 * 1024) << "MB" << endl;
+				cout << "Clock Freq: " << d.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>() << "MHz" << endl;
+				cout << "Available: " << (d.getInfo<CL_DEVICE_AVAILABLE>() ? "True" : "False") << endl;
+			}
+
+			cout << endl;
+			cout << "********************" << endl;
+		}
+	}
+	catch (Error error)
+	{
+		cout << error.what() << "(" << error.err() << ")" << endl;
+	}
+	return 0;
+	/***********************************/
 }
